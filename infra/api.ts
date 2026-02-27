fa5da6c4f00fab5dd436c336ccb95a8421ab2a05
DariaV13 <daria.vyskubina@jam3.com>
2026-02-27 14:47:35 -0500

    Adding the API

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,18 @@
-import { bucket } from './storage';
+import { table } from './storage';
 
-export const api = new sst.aws.ApiGatewayV2('Api');
-
-api.route('GET /', {
-  link: [bucket],
-  handler: 'packages/functions/src/api.handler',
+// Create the API
+export const api = new sst.aws.ApiGatewayV2('Api', {
+  transform: {
+    route: {
+      handler: {
+        link: [table],
+      },
+    },
+  },
 });
+
+api.route('POST /practice', 'packages/functions/src/create.main');
+api.route('GET /practice/{id}', 'packages/functions/src/get.main');
+api.route('GET /practice', 'packages/functions/src/list.main');
+api.route('PUT /practice/{id}', 'packages/functions/src/update.main');
+api.route('DELETE /practice/{id}', 'packages/functions/src/delete.main');
